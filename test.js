c8923f2252e887b534b7d9734319f2fddb0a277e
Oleg Ostaf <education@Antons-Laptop.local>
2024-12-03 19:42:14 +0200

    wrong test result

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const reverseString = require('./index');
 
 test('Інвертує рядок правильно', () => {
-    expect(reverseString("hello")).toBe("olleh");
+    expect(reverseString("hello")).toBe("ollesh");
     expect(reverseString("12345")).toBe("54321");
 });
